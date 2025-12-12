3f2b2ee446e78dedcf3a2a14e562dc16d793f3a1
Armando14-ux <a24110273@live.ceti.mx>
2025-12-11 23:30:28 -0600

    atras

--- src/main.cpp
+++ src/main.cpp
@@ -493,7 +493,7 @@ public:
         speedLabel.setFillColor(sf::Color::Yellow);
         window.draw(speedLabel);
         
-        int yPos = 10;  // Un poco de espacio antes de las manzanas
+        for (int i = 0; i < speedLevel && i < 8; i++) {
             sf::RectangleShape speedBar(sf::Vector2f(8, 12));
             speedBar.setPosition(panelX + i * 10, yPos + 8);
             speedBar.setFillColor(sf::Color::Yellow);
